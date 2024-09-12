8bdb0ac15aaf71ab3b6d4dd3d983338e83430d80
Louise <louloulam1606@gmail.com>
2024-09-12 18:59:35 +0200

    Adding documentation to analysis.py.

--- results/analysis.py
+++ results/analysis.py
@@ -1,5 +1,6 @@
+"""This script is used to analyze the profiling results of the code."""
+
 import pstats
 
 stats = pstats.Stats("profiling_results.prof")
-
 stats.sort_stats("cumulative").print_stats(10)
